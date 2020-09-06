c08da25c00bfe6232dce932ef7e451b44af95301
Jordan <jordan.marshall@getbuilt.com>
2020-09-06 14:26:12 -0500

    remove old web assets folder, using build folder now

--- react-ionic/spoopy/static/js/53.f4204cb1.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpreact-ionic"]=this["webpackJsonpreact-ionic"]||[]).push([[53],{135:function(n,t,e){"use strict";e.r(t),e.d(t,"startStatusTap",(function(){return o}));var i=e(3),o=function(){var n=window;n.addEventListener("statusTap",(function(){Object(i.j)((function(){var t=n.innerWidth,e=n.innerHeight,o=document.elementFromPoint(t/2,e/2);if(o){var c=o.closest("ion-content");c&&c.componentOnReady().then((function(){Object(i.g)((function(){return c.scrollToTop(300)}))}))}}))}))}}}]);
-//# sourceMappingURL=53.f4204cb1.chunk.js.map
